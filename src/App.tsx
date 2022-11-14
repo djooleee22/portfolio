fff0ded36b1c55a794b574bbefbb47c8a3bc7611
djooleee22 <antonijevic.dj1@gmail.com>
2022-11-14 14:35:17 +0100

    added animations

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,33 @@ import About from "./Components/About";
 import Experience from "./Components/Experience";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
+  let location = useLocation();
   return (
     <div id="App">
       <Header />
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <About />
-          </Route>
-          <Route path="/experience">
-            <Experience />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
+      <TransitionGroup component={"main"}>
+        <CSSTransition timeout={500} classNames="fade" key={location.key}>
+          <Switch location={location}>
+            <Route exact path="/">
+              <About />
+            </Route>
+            <Route path="/experience">
+              <Experience />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
