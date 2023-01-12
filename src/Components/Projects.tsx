6397756f675319a3d5c7282be1a73038e89a3a5d
djooleee22 <antonijevic.dj1@gmail.com>
2023-01-12 13:36:25 +0100

    added TvLibrary project

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,78 +1,78 @@
 import React from "react";
 import "./Projects.scss";
-import quiz from "../slike/Quiz.png";
+import quiz from "../slike/quizNewImage.png";
 import bringMe from "../slike/Bring Me.png";
 import bitShow from "../slike/BitShow.png";
 import giphy from "../slike/Giphy.png";
 import rickAndMorty from "../slike/Rick and Morty.png";
 import youtubeClone from "../slike/Youtube clone.png";
 import splitter from "../slike/Splitter.png";
-import chatRoom from "../slike/Chatroom.png";
 import weather from "../slike/Weather.png";
+import tvLibrary from "../slike/tvLibrary.png";
 
 const projectsList = [
+  {
+    id: "p8",
+    name: "TV Library",
+    image: tvLibrary,
+    num: "01",
+    url: "https://movies-tmdb-omega.vercel.app",
+  },
   {
     id: "p1",
     name: "Quiz",
     image: quiz,
-    num: "01",
+    num: "02",
     url: "https://quiz-app-lake-xi.vercel.app",
   },
   {
     id: "p9",
     name: "Weather app",
     image: weather,
-    num: "02",
+    num: "03",
     url: "https://weather-app-alpha-two-14.vercel.app",
   },
   {
     id: "p2",
     name: "Bring Me!",
     image: bringMe,
-    num: "03",
+    num: "04",
     url: "https://food-order-app-sooty.vercel.app",
   },
   {
     id: "p3",
     name: "BitShow",
     image: bitShow,
-    num: "04",
+    num: "05",
     url: "https://bit-show-one.vercel.app",
   },
   {
     id: "p4",
     name: "Giphy",
     image: giphy,
-    num: "05",
+    num: "06",
     url: "https://giphy-plum.vercel.app",
   },
   {
     id: "p5",
     name: "Rick and Morty",
     image: rickAndMorty,
-    num: "06",
+    num: "07",
     url: "https://rick-morty-green.vercel.app",
   },
   {
     id: "p6",
     name: "YouTube Clone",
     image: youtubeClone,
-    num: "07",
+    num: "08",
     url: "https://youtube-clone-alpha-woad.vercel.app",
   },
   {
     id: "p7",
     name: "Splitter",
     image: splitter,
-    num: "08",
-    url: "https://splitter-jet.vercel.app",
-  },
-  {
-    id: "p8",
-    name: "Chatroom",
-    image: chatRoom,
     num: "09",
-    url: "https://chat-room-blond.vercel.app",
+    url: "https://splitter-jet.vercel.app",
   },
 ];
 
